8f7d9d84729e0dc934dc6372127ae6d8cd257d5a
aRandomDeveloper <jacksim2121@gmail.com>
2021-01-03 11:39:28 +0800

    fix requred jsonfile module

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const dotenv = require('dotenv');
-const JSONIFIER = require("jsonfile");
 const fPath = "./"
 //one time declare files
 const fileFunc = require(fPath + "fileFunc.js")
@@ -25,7 +24,7 @@ const fileFunc = require(fPath + "fileFunc.js")
 
 dotenv.config();
 //--------------------------------ONE TIME TOGGLE FOR BETA------------------------------------
-var betaTesting = true;
+var betaTesting = false;
 //--------------------------------------------------------------------------------------------
 var PREFIX;
 var token;
